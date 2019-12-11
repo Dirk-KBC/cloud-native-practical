22d9a53d60b07ac42b8d365d8ae38144695cab62
Dirk Vanderheyden <dirk.vanderheyden@kbc.be>
2019-12-11 09:13:29 +0100

    added: getShoppingList(@PathVariable(id) String id)

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/ShoppingListController.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/ShoppingListController.java
@@ -33,13 +33,43 @@ public class ShoppingListController {
         List<String> response = new ArrayList<String>();
 
         // for each cocktail in the request, add it to the shoppingList with id "{id}"
-        requestBody.forEach((cocktailId)->response.add(cocktailId.toString()));
+        requestBody.forEach((cocktailId)->response.add(cocktailId.getCocktailId()));
      /*   for (String cocktailId : requestBody.getCocktailId()) {
             // TODO: add cocktail to an _existing_ shoppinglist with id = {id}
             // then, add the cocktailId to the response-object
             response.add(cocktailId);
         }*/
 
+        return response;
+    }
+
+
+    // Request: GET http://localhost:8080/shopping-lists/eb18bb7c-61f3-4c9f-981c-55b1b8ee8915
+    // Response: 200 OK
+    //{
+    //    "shoppingListId": "90689338-499a-4c49-af90-f1e73068ad4f",
+    //    "name": "Stephanie's birthday",
+    //    "ingredients": [
+    //        "Tequila",
+    //        "Triple sec",
+    //        "Lime juice",
+    //        "Salt",
+    //        "Blue Curacao"
+    //    ]
+    //}
+
+    @GetMapping(value = "/shopping-lists/{id}", produces = "application/json")
+    public ShoppingListResponse getShoppingList(@PathVariable("id") String id) {
+        List<String> ingredients = new ArrayList<>();
+        ingredients.add("Tequila");
+        ingredients.add("Triple sec");
+        ingredients.add("Lime juice");
+        ingredients.add("Salt");
+        ingredients.add("Blue Curacao");
+        ShoppingListResponse response = new ShoppingListResponse();
+        response.setShoppingListId(id);
+        response.setName("Stephanie's birthday");
+        response.setIngredients(ingredients);
         return response;
     }
 }
---
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/ShoppingListResponse.java
@@ -0,0 +1,50 @@
+package com.ezgroceries.shoppinglist;
+
+import java.util.List;
+
+// {
+//    "shoppingListId": "90689338-499a-4c49-af90-f1e73068ad4f",
+//    "name": "Stephanie's birthday",
+//    "ingredients": [
+//        "Tequila",
+//        "Triple sec",
+//        "Lime juice",
+//        "Salt",
+//        "Blue Curacao"
+//    ]
+//}
+public class ShoppingListResponse {
+
+
+    private String shoppingListId;
+    private String name;
+    private List<String> ingredients;
+
+    public ShoppingListResponse() {
+    }
+
+    public String getShoppingListId() {
+        return shoppingListId;
+    }
+
+    public void setShoppingListId(String shoppingListId) {
+        this.shoppingListId = shoppingListId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+}
+
