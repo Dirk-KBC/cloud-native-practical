3d5b4e92cea33df2fc4f92fc9c86f13116388e55
Dirk Vanderheyden <dirk.vanderheyden@kbc.be>
2019-12-18 14:34:02 +0100

    lab05 completed

---
+++ shopping-list/src/main/resources/db/migration/V0.2__Add_Cocktail_Table.sql
@@ -0,0 +1,6 @@
+create table COCKTAIL (
+                          ID UUID PRIMARY KEY,
+                          ID_DRINK UUID,
+                          NAME TEXT,
+                          INGREDIENTS TEXT
+);
